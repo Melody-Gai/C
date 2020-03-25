7faf970a90928e7f0d734b502d6e835c3ce04211
Melody-Gai <539263156@qq.com>
2020-03-25 22:40:42 +0800

    C++

---
+++ 20200324/Test.cpp
@@ -0,0 +1,34 @@
+/*
+#include<iostream>  //一个简单的C++程序
+using namespace std;  //命名空间
+
+class My_student
+{
+	class boy
+	{
+		char boy_name[20];
+		int boy_age;
+	}my_boy_student;
+	class girl
+	{
+		char girl_name[20];
+		int girl_age;
+	}my_girl_student;
+public:
+	void student_input(void);
+	void student_output(void);
+};
+
+int main()
+{
+	cout << "hello" << std::endl;
+	//std::endl换行符 
+	char str_greet[] = "welcome!";
+	cout << str_greet << endl;
+	getchar();
+
+	double radius;
+	std::cin >> radius;  //标准输入流对象cin  输入运算符>>
+	cout << 3.14 * radius * radius;
+}
+*/
